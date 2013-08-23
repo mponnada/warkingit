a0848a8ce8ebc114eee0be2352fabc20293922af
gogukt <mohankuldeepis@gmail.com>
2013-08-23 20:01:11 +0000

    Fixed web.js

--- web.js
+++ web.js
@@ -21,6 +21,26 @@ app.get('/courses', function(request, response) {
       response.send(data);
     });
 
+app.get('/the-fun-side-of-programming', function(request, response) {
+      var data = fs.readFileSync('the_fun_side_of_programming.html').toString();
+      response.send(data);
+    });
+
+app.get('/the-frontend-ui-developer', function(request, response) {
+      var data = fs.readFileSync('the_frontend_ui_developer.html').toString();
+      response.send(data);
+    });
+
+app.get('/the-server-side-ninja', function(request, response) {
+      var data = fs.readFileSync('the_server_side_ninja.html').toString();
+      response.send(data);
+    });
+
+app.get('/mobile-app-in-3-days', function(request, response) {
+      var data = fs.readFileSync('mobile_app_in_3_days.html').toString();
+      response.send(data);
+    });
+
 
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
