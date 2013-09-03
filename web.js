f8cc5c558670b71d70f0a570ef2dcb13bf446139
gogukt <mohankuldeepis@gmail.com>
2013-09-03 14:38:13 +0000

    Fixed web.js for sendgrid

--- web.js
+++ web.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var fs = require('fs');
-var Sendgrid = require("sendgrid-web");
+//var Sendgrid = require("sendgrid-web");
 
 
 var app = express.createServer(express.logger());
